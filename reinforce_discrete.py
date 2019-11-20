9f76f848fb0083e003236c8121ed5f650f26febc
blyucs <53803532+blyucs@users.noreply.github.com>
2019-11-20 16:09:01 +0800

    temp upload

--- reinforce_discrete.py
+++ reinforce_discrete.py
@@ -31,13 +31,13 @@ class REINFORCE:
     def __init__(self, hidden_size, num_inputs, action_space):
         self.action_space = action_space
         self.model = Policy(hidden_size, num_inputs, action_space)
-	self.model = self.model.cuda()
+	    #self.model = self.model.cuda()
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
-	self.model.train()
+        self.model.train()
 
     def select_action(self, state):
-        probs = self.model(Variable(state).cuda())       
-        action = probs.multinomial().data
+        probs = self.model(Variable(state))
+        action = probs.multinomial(1).data
         prob = probs[:, action[0,0]].view(1, -1)
         log_prob = prob.log()
         entropy = - (probs*probs.log()).sum()
@@ -49,10 +49,11 @@ class REINFORCE:
         loss = 0
         for i in reversed(range(len(rewards))):
             R = gamma * R + rewards[i]
-            loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i])).cuda()).sum() - (0.0001*entropies[i].cuda()).sum()
+            #loss = loss - (log_probs[i]*(Variable(R).expand_as(log_probs[i]))).sum() - (0.0001*entropies[i]).sum()
+            loss = loss - (log_probs[i] * (Variable(R))) - (0.0001 * entropies[i]).sum()
         loss = loss / len(rewards)
-		
+
         self.optimizer.zero_grad()
         loss.backward()
-	utils.clip_grad_norm(self.model.parameters(), 40)
+        utils.clip_grad_norm(self.model.parameters(), 40)
         self.optimizer.step()
